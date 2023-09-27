6d34d5cc49d9de0c291451332a4e27a22b929bfa
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-27 12:24:59 -0600

    begining associations, models

---
+++ models/index.js
@@ -0,0 +1,40 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  // through:{
+  //   model: Tag,
+  //   unique: false
+  // },
+  // as: 'productCategories'
+});
+
+// Categories have many Products
+Category.hasMany(Product,{
+//   through: {
+//   model : Tag,
+//   unique: false
+// },
+// as: 'categoriesProducts'
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
